498f926139dcda761034348e250c0214a3af32c8
Nguyễn Tiến Đạt <tiwen.datt332@gmail.com>
2025-10-18 10:24:33 +0700

    MovableObject

---
+++ src/MovableObject.java
@@ -0,0 +1,39 @@
+public abstract class MovableObject extends GameObject {
+    protected int dx;
+    protected int dy;
+
+    public MovableObject(int x, int y, int width, int height, int dx, int dy) {
+        super(x, y, width, height);
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public void move() {
+        x += dx;
+        y += dy;
+    }
+
+    @Override
+    public void update() {
+        move();
+    }
+
+    @Override
+    public abstract void render();
+
+    public int getDx() {
+        return dx;
+    }
+
+    public void setDx(int dx) {
+        this.dx = dx;
+    }
+
+    public int getDy() {
+        return dy;
+    }
+
+    public void setDy(int dy) {
+        this.dy = dy;
+    }
+}
