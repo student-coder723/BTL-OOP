81e8392c5030f5bc133ee9a1ee79888830f46059
hoanganh <hoanganh16e@gmail.com>
2025-10-19 11:04:26 +0700

    Item

---
+++ src/Fast.java
@@ -0,0 +1,16 @@
+public class Fast
+{
+
+    Fast() {}
+
+    public static void checkFast(icons icons_obj ,Paddle paddle_obj , Ball  ball_obj )
+    {
+        if(icons_obj.i_fast.getBoundsInParent().intersects(paddle_obj.paddle_iv.getBoundsInParent()) )
+        {
+            Sound.playsound_capsule();
+            icons_obj.i_fast.setImage(null);
+            ball_obj.setSpeed(7);
+
+        }
+    }
+}
