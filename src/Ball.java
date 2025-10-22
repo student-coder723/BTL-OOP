616b7786dfec01b7f4ca1bbd0c39fda6b181005d
Doanh36 <doanhabcxyz2006@gmail.com>
2025-10-22 15:10:39 +0700

    phóng bóng ra khỏi paddle

--- src/Ball.java
+++ src/Ball.java
@@ -1,19 +1,21 @@
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-
 public class Ball {
     Image ballImage = new Image(this.getClass().getResource("/Ball/ball.png").toExternalForm(), (double)30.0F, (double)30.0F, false, false);
     ImageView ball_iv;
     private double Height;
     private double Width;
+    private double dx;
+    private double dy;
 
     public Ball() {
         this.ball_iv = new ImageView(this.ballImage);
         this.Height = (double)48.0F;
         this.Width = (double)50.0F;
+        this.dx = 0;
+        this.dy = 0;
     }
-
     public double getHeight() {
         return this.Height;
     }
@@ -44,5 +46,21 @@ public class Ball {
 
     public void setY(double y) {
         this.ball_iv.setY(y);
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public void setDx(double dx) {
+        this.dx = dx;
+    }
+
+    public double getDy() {
+        return dy;
+    }
+
+    public void setDy(double dy) {
+        this.dy = dy;
     }
 }
