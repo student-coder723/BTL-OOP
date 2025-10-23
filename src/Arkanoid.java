9941a72eff9bb8b5f6e16c96430752ac85607161
Nguyễn Tiến Đạt <tiwen.datt332@gmail.com>
2025-10-23 18:38:51 +0700

    Render background

--- src/Arkanoid.java
+++ src/Arkanoid.java
@@ -1,17 +1,50 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Arkanoid extends Application {
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+    private Image background;
 
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Arkanoid Game");
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, 800, 600);
+
+        try {
+            background = new Image(getClass().getResourceAsStream("/background.jpg"));
+        } catch (Exception e) {
+            System.out.println("Error");
+            e.printStackTrace();
+            background = null;
+        }
+
+        Pane root = new Pane();
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        root.getChildren().add(canvas);
+        Scene scene = new Scene(root);
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        renderGame(gc);
+
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
+    }
+
+    private void renderGame(GraphicsContext gc) {
+        if (background != null) {
+            gc.drawImage(background, 0, 0, WIDTH, HEIGHT);
+        } else {
+            gc.setFill(Color.WHITE);
+            gc.fillRect(0, 0 ,WIDTH, HEIGHT);
+        }
     }
 
     public static void main(String[] args) {
