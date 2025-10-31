466e429376218a9b76b41317e666c003d63be70e
IT newbie <hoanganh16e@gmail.com>
2025-10-31 16:05:30 +0700

    vẽ paddle đơn giản

--- src/Arkanoid.java
+++ src/Arkanoid.java
@@ -8,11 +8,11 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 
-public class Arkanoid extends Application {
-
-
     private static final int SCENE_WIDTH = 800;
     private static final int SCENE_HEIGHT = 600;
+
+    private GameUI gameUI;
+    private Paddle paddle;
 
     @Override
     public void start(Stage primaryStage) {
@@ -31,14 +31,17 @@ public class Arkanoid extends Application {
 
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
+        gameUI = new GameUI();
+        paddle = new Paddle(SCENE_WIDTH / 2 - 50, SCENE_HEIGHT - 40, 100, 20, 5)
+
         AnimationTimer gameLoop = new AnimationTimer() {
             @Override
             public void handle(long now) {
-                gc.setFill(Color.BLACK);
-                gc.fillRect(0, 0, SCENE_WIDTH, SCENE_WIDTH);
+                gameUI.render(gc, SCENE_WIDTH, SCENE_HEIGHT);
             }
         };
 
+        gameLoop.start();
         primaryStage.show();
     }
 
--- src/GameUI.java
+++ src/GameUI.java
@@ -1,11 +1,26 @@
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 public class GameUI {
+    private Paddle paddle;
     public GameUI() {
 
     }
 
-    public void render(GraphicsContext gc) {
+    public void render(GraphicsContext gc, int width, int height) {
+        gc.setFill(Color.BLACK);
+        gc.fillRect(0, 0, width, height);
 
+        if (paddle != null){
+            paddle.render(gc);
+        }
+    }
+
+    public Paddle getPaddle(){
+        return paddle;
+    }
+
+    public void setPaddle(Paddle paddle) {
+        this.paddle = paddle;
     }
 }
