c7ef45fbf5d1308b126838b7a0f78f3de1568dcd
Doanh36 <doanhabcxyz2006@gmail.com>
2025-10-31 20:15:05 +0700

    Cập nhật brick kế thừa gameObject

--- src/Brick.java
+++ src/Brick.java
@@ -1,2 +1,27 @@
-public class Brick {
+import javafx.scene.canvas.GraphicsContext;
+
+public class Brick extends GameObject {
+    private boolean isDestroyed;
+
+    public Brick(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public void update() {
+
+
+    }
+
+    @Override
+    public void render(GraphicsContext gc) {
+    }
+
+    public boolean isDestroyed() {
+        return isDestroyed;
+    }
+
+    public void setDestroyed(boolean destroyed) {
+        isDestroyed = destroyed;
+    }
 }
