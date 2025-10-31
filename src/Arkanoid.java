031e4e5dfd9e5d460946818f8c845ce8485a9281
Nguyễn Tiến Đạt <tiwen.datt332@gmail.com>
2025-10-31 17:46:50 +0700

    fix: render background

--- src/Arkanoid.java
+++ src/Arkanoid.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.animation.AnimationTimer;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.scene.canvas.Canvas;
@@ -13,27 +14,28 @@ public class Arkanoid extends Application {
 
     private GameUI gameUI;
     private Paddle paddle;
+    private Image backgroundImage;
 
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Arkanoid Game");
 
-
         Pane root = new Pane();
         Canvas canvas = new Canvas(SCENE_WIDTH, SCENE_HEIGHT);
         root.getChildren().add(canvas);
-
 
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
 
-
         GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        backgroundImage = ResourceLoader.loadImage("/Background/Background.png");
 
         gameUI = new GameUI();
         paddle = new Paddle(SCENE_WIDTH / 2 - 50, SCENE_HEIGHT - 40, 100, 20, 5);
 
+        gameUI.setBackgroundImage(backgroundImage);
         gameUI.setPaddle(paddle);
 
         AnimationTimer gameLoop = new AnimationTimer() {
