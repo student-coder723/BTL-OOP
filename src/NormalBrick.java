284f7aaaffd1a0fc66842980fd814025928b1f94
Doanh36 <doanhabcxyz2006@gmail.com>
2025-10-31 22:08:34 +0700

    tạo lớp normalbrick kế thừa từ brick

---
+++ src/NormalBrick.java
@@ -0,0 +1,15 @@
+import javafx.scene.canvas.GraphicsContext;
+
+public class NormalBrick extends Brick {
+    public NormalBrick(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+    @Override
+    public void update() {
+    }
+
+    @Override
+    public void render(GraphicsContext gc) {
+    }
+}
+
