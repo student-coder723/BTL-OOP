a83895acf6559c6910ef8bd73b6ea7ad3e0e706b
Doanh36 <doanhabcxyz2006@gmail.com>
2025-10-31 22:26:34 +0700

    Tạo class strongbrick kế thừa brick

---
+++ src/StrongBrick.java
@@ -0,0 +1,19 @@
+import javafx.scene.canvas.GraphicsContext;
+
+public class StrongBrick extends Brick {
+    public StrongBrick(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public void update() {
+
+
+    }
+
+    @Override
+    public void render(GraphicsContext g) {
+
+
+    }
+}
