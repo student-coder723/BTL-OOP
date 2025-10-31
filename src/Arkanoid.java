671273bb29c8ee382e541d4a8e087a9ba2ad1556
Doanh36 <doanhabcxyz2006@gmail.com>
2025-10-31 18:02:50 +0700

    merge (#28)
    * vẽ paddle đơn giản
    
    * thêm paddle màu trắng
    
    * Render background
    
    * fix: render background
    
    * fix: render background
    
    * Update GameUI.java
    
    ---------
    
    Co-authored-by: IT newbie <hoanganh16e@gmail.com>
    Co-authored-by: Nguyễn Tiến Đạt <tiwen.datt332@gmail.com>

--- src/Arkanoid.java
+++ src/Arkanoid.java
@@ -1,18 +1,20 @@
 import javafx.application.Application;
 import javafx.animation.AnimationTimer;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
-
 public class Arkanoid extends Application {
-
-
     private static final int SCENE_WIDTH = 800;
     private static final int SCENE_HEIGHT = 600;
+
+    private GameUI gameUI;
+    private Paddle paddle;
+    private Image backgroundImage;
 
     @Override
     public void start(Stage primaryStage) {
@@ -22,22 +24,28 @@ public class Arkanoid extends Application {
         Canvas canvas = new Canvas(SCENE_WIDTH, SCENE_HEIGHT);
         root.getChildren().add(canvas);
 
-
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
 
-
         GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        backgroundImage = ResourceLoader.loadImage("/Background/Background.png");
+
+        gameUI = new GameUI();
+        paddle = new Paddle(SCENE_WIDTH / 2 - 50, SCENE_HEIGHT - 40, 100, 20, 5);
+
+        gameUI.setBackgroundImage(backgroundImage);
+        gameUI.setPaddle(paddle);
 
         AnimationTimer gameLoop = new AnimationTimer() {
             @Override
             public void handle(long now) {
-                gc.setFill(Color.BLACK);
-                gc.fillRect(0, 0, SCENE_WIDTH, SCENE_WIDTH);
+                gameUI.render(gc, SCENE_WIDTH, SCENE_HEIGHT);
             }
         };
 
+        gameLoop.start();
         primaryStage.show();
     }
 
--- src/GameUI.java
+++ src/GameUI.java
@@ -1,19 +1,39 @@
-import javafx.scene.Node;
-import javafx.scene.layout.Pane;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 
 public class GameUI {
+    private Paddle paddle;
+    private Image backgroundImage;
+    public GameUI() {}
 
-    private Pane rootPane;
 
-    public GameUI(Pane root) {
-        this.rootPane = root;
+    public void render(GraphicsContext gc, int width, int height) {
+        if (backgroundImage != null) {
+            gc.drawImage(backgroundImage, 0, 0, width, height);
+        } else {
+            gc.setFill(Color.BLACK);
+            gc.fillRect(0, 0, width, height);
+        }
+
+        if (paddle != null){
+            paddle.render(gc);
+        }
     }
 
-    public void addObject(Node node) {
-        rootPane.getChildren().add(node);
+    public Paddle getPaddle(){
+        return paddle;
     }
 
-    public void removeObject(Node node) {
-        rootPane.getChildren().remove(node);
+    public void setPaddle(Paddle paddle) {
+        this.paddle = paddle;
+    }
+
+    public Image getBackgroundImage() {
+        return backgroundImage;
+    }
+
+    public void setBackgroundImage(Image backgroundImage) {
+        this.backgroundImage = backgroundImage;
     }
 }
