82f774e0bfc8f9819b9fc8a48c3e12788eab798a
Doanh36 <doanhabcxyz2006@gmail.com>
2025-11-01 00:15:06 +0700

    Vẽ các khối gạch hình chữ nhật màu trắng đơn giản

--- src/GameUI.java
+++ src/GameUI.java
@@ -1,13 +1,17 @@
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
-import javafx.scene.image.Image;
+import java.util.List;
+import java.util.ArrayList;
 
 public class GameUI {
     private Paddle paddle;
+    private Ball ball;
     private Image backgroundImage;
-    public GameUI() {}
-
+    private List<Brick> bricks;
+    public GameUI() {
+        this.bricks = new ArrayList<>();
+    }
 
     public void render(GraphicsContext gc, int width, int height) {
         if (backgroundImage != null) {
@@ -20,6 +24,15 @@ public class GameUI {
         if (paddle != null){
             paddle.render(gc);
         }
+
+        if (ball != null) {
+            ball.render(gc);
+        }
+
+        for (Brick brick : bricks) {
+            brick.render(gc);
+        }
+
     }
 
     public Paddle getPaddle(){
@@ -30,6 +43,14 @@ public class GameUI {
         this.paddle = paddle;
     }
 
+    public Ball getBall() {
+        return ball;
+    }
+
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
     public Image getBackgroundImage() {
         return backgroundImage;
     }
@@ -37,4 +58,9 @@ public class GameUI {
     public void setBackgroundImage(Image backgroundImage) {
         this.backgroundImage = backgroundImage;
     }
+
+    public void setBricks(List<Brick> bricks) {
+        this.bricks = bricks;
+    }
+
 }
