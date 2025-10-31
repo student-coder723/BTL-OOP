03bb644abb9b09d73e5bfba9a083819c8b16a947
Nguyễn Tiến Đạt <tiwen.datt332@gmail.com>
2025-10-31 10:12:39 +0700

    Update MovableObject (#14)

--- src/MovableObject.java
+++ src/MovableObject.java
@@ -1,2 +1,41 @@
-public class MovableObject {
+import javafx.scene.canvas.GraphicsContext;
+
+public abstract class MovableObject extends GameObject {
+    protected int dx;
+    protected int dy;
+
+    public MovableObject(int x, int y, int width, int height, int dx, int dy) {
+        super(x, y, width, height);
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public void move() {
+        x += dx;
+        y += dy;
+    }
+
+    @Override
+    public void update() {
+        move();
+    }
+
+    @Override
+    public abstract void render(GraphicsContext g);
+
+    public int getDx() {
+        return dx;
+    }
+
+    public void setDx(int dx) {
+        this.dx = dx;
+    }
+
+    public int getDy() {
+        return dy;
+    }
+
+    public void setDy(int dy) {
+        this.dy = dy;
+    }
 }
