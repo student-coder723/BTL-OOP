cfa07d75dcdfc3e36d42884965bb33ba6282d00c
Nguyễn Tiến Đạt <tiwen.datt332@gmail.com>
2025-11-01 20:57:09 +0700

    update GameState: click chuột để bắt đầu chơi

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -4,11 +4,13 @@ public class InputHandler {
     private final Scene scene;
     private final Paddle paddle;
     private final int sceneWidth;
+    private final GameLogic gameLogic;
 
-    public InputHandler(Scene scene, Paddle paddle, int sceneWidth) {
+    public InputHandler(Scene scene, Paddle paddle, int sceneWidth, GameLogic gameLogic) {
         this.scene = scene;
         this.paddle = paddle;
         this.sceneWidth = sceneWidth;
+        this.gameLogic = gameLogic;
     }
 
     public void registerHandlers() {
@@ -23,6 +25,10 @@ public class InputHandler {
                 newX = maxPaddleX;
             }
             paddle.setX(newX);
+        });
+
+        scene.setOnMouseClicked(event -> {
+            gameLogic.startGame();
         });
     }
 }
