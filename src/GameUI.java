365944ca7ee640fe69935ed2d70456b093ed6c4a
hoanganh <hoanganh16e@gmail.com>
2025-11-01 22:37:13 +0700

    thêm điểm cho game

--- src/GameUI.java
+++ src/GameUI.java
@@ -1,6 +1,8 @@
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -9,11 +11,21 @@ public class GameUI {
     private Ball ball;
     private Image backgroundImage;
     private List<Brick> bricks;
+    private Font uiFont;
+
     public GameUI() {
         this.bricks = new ArrayList<>();
+
+        try {
+            this.uiFont = Font.font("Verdana", 20);
+        } catch (Exception e) {
+            System.err.println("Lỗi");
+            this.uiFont = Font.font(20);
+        }
+
     }
 
-    public void render(GraphicsContext gc, int width, int height) {
+    public void render(GraphicsContext gc, int width, int height, int score, GameState state) {
         if (backgroundImage != null) {
             gc.drawImage(backgroundImage, 0, 0, width, height);
         } else {
@@ -21,16 +33,33 @@ public class GameUI {
             gc.fillRect(0, 0, width, height);
         }
 
-        if (paddle != null){
-            paddle.render(gc);
-        }
+        if (state == GameState.READY || state == GameState.RUNNING) {
+            if (paddle != null) {
+                paddle.render(gc);
+            }
 
-        if (ball != null) {
-            ball.render(gc);
+            if (ball != null) {
+                ball.render(gc);
+            }
         }
-
         for (Brick brick : bricks) {
             brick.render(gc);
+        }
+
+        gc.setFill(Color.WHITE);
+        gc.setFont(uiFont);
+
+
+        gc.setTextAlign(TextAlignment.LEFT);
+        gc.fillText("Score: " + score, 10, 30);
+
+
+        gc.setTextAlign(TextAlignment.CENTER);
+
+
+        if (state == GameState.READY) {
+            gc.setFill(Color.YELLOW);
+            gc.fillText("Click to Start", width / 2.0, height / 2.0);
         }
 
     }
