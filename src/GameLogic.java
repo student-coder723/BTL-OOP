64f10aa3b00da70cb44419f3b8ed9eb9fe791521
Nguyễn Tiến Đạt <tiwen.datt332@gmail.com>
2025-11-01 15:55:45 +0700

    feat: xử lý va chạm bóng và paddle

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,3 +1,24 @@
 public class GameLogic {
+    private Ball ball;
+    private Paddle paddle;
 
+    public GameLogic(Ball ball, Paddle paddle) {
+        this.ball = ball;
+        this.paddle = paddle;
+    }
+
+    public void update() {
+        ball.update();
+        paddle.update();
+        checkCollisions();
+    }
+
+    private void checkCollisions() {
+        if (ball.checkCollision(paddle)) {
+            if (ball.getDirectionY() > 0) {
+                ball.reverseDirectionY();
+                ball.setY(paddle.getY() - ball.getHeight() - 1);
+            }
+        }
+    }
 }
