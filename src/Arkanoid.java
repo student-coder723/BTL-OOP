f0a587878392cb5c68912fd2a9bdc39e2c256abe
Nguyễn Tiến Đạt <tiwen.datt332@gmail.com>
2025-11-03 16:54:14 +0700

    fix: đồng bộ main và TienDat2

--- src/Arkanoid.java
+++ src/Arkanoid.java
@@ -126,7 +126,6 @@ public class Arkanoid extends Application {
 
         for (int i = 0; i < brickRows; i++) {
             for (int j = 0; j < brickCols; j++) {
-
                 int x = offsetLeft + j * (brickWidth + padding);
                 int y = offsetTop + i * (brickHeight + padding);
 
